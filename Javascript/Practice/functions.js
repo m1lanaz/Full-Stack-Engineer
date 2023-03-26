aa7df16a0ffe1a0ecc71d13ba227a03e3020c0b2
Milana <milana.zaborovska@gmail.com>
2023-03-26 23:18:09 +0100

    complete practice with functions

---
+++ Javascript/Practice/functions.js
@@ -0,0 +1,12 @@
+function logger() {
+    console.log('My name is Milana');
+};
+
+logger();
+
+function shoppingList(item1, item2) {
+    const list = (`I need to get ${item1} and ${item2}!`);
+    return list
+};
+
+console.log(shoppingList('apples', 'oranges'));
