72e11599f3fb7e2b29077951355b41ddb3f396ad
Milana <milana.zaborovska@gmail.com>
2023-03-27 17:29:06 +0100

    assignement function complete

---
+++ Javascript/Practice/arrow functions.js
@@ -0,0 +1,5 @@
+const calcAge = birthyear => 2037 - birthyear;
+// return happens explicintly
+
+const greeting = (firstName, secondName) => `Hello ${firstName} ${secondName}!`;
+console.log(greeting('Mil', 'Zab'));
