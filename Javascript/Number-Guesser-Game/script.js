898a9eaf395ad7e95cbee6deb267a2e9cdf61fa8
Milana <milana.zaborovska@gmail.com>
2023-03-28 22:36:58 +0100

    lesson 73

--- Javascript/Number-Guesser-Game/script.js
+++ Javascript/Number-Guesser-Game/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    if(!guess){
+        console.log("no input given!");
+    }
+});
