def79d443e2928e4b340d25f85e82450c798d4de
Milana <milana.zaborovska@gmail.com>
2023-04-07 00:13:12 +0100

    complete for of loop

---
+++ Javascript/Data-Structures/Practice/forof loop.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex],  this.mainMenu[mainIndex]];
+    },
+  
+    openingHours: {
+      thu: {
+        open: 12,
+        close: 22,
+      },
+      fri: {
+        open: 11,
+        close: 23,
+      },
+      sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+      },
+    },
+  };
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [item,element] of menu.entries()) {
+    console.log(`${item + 1}: ${element}`);
+}
+
+console.log([...menu.entries()]); // 2D array that contains position and the item
