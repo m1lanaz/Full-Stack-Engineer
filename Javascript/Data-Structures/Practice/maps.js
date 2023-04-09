ecdefd9d24e9043b34308c95d22a9700a1e6b53e
Milana <milana.zaborovska@gmail.com>
2023-04-09 13:53:40 +0100

    maps complete

---
+++ Javascript/Data-Structures/Practice/maps.js
@@ -0,0 +1,24 @@
+const restMap = new Map();
+restMap.set('owner', 'Mil');
+restMap.set('name', 'Clacio resturant');
+console.log(restMap);
+
+restMap
+.set('categories', ['Italian', 'Pizzeria', 'Organic'])
+.set('close', 23)
+.set(true, 'We are open');
+
+console.log(restMap);
+
+console.log(restMap.get('categories'));
+
+console.log(restMap.has('categories'));
+restMap.delete('name');
+console.log(restMap);
+
+restMap.clear();
+console.log(restMap.size);
+
+const arr = [1,2]
+restMap.set(arr, 'Test');
+console.log(restMap.get(arr));
