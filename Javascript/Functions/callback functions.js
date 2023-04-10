eb36e11eee5b2ccb43f3303c8e4261ceaa65f3fe
Milana <milana.zaborovska@gmail.com>
2023-04-10 16:44:13 +0100

    complete callback functions

---
+++ Javascript/Functions/callback functions.js
@@ -0,0 +1,15 @@
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function(str, fn) {
+    console.log(`Transformed string: ${fn(str)}`);
+}
+
+transformer('Javascript is the best!', upperFirstWord);
