e850f3eaf5ab307e0191c8a51c1506b7fed6d2c5
Milana <milana.zaborovska@gmail.com>
2023-04-17 19:51:47 +0100

    complete for each with maps and sets

---
+++ Javascript/Arrays/forEach with maps and sets.js
@@ -0,0 +1,20 @@
+// MAP
+
+const currencies = new Map([
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function(value, key, map){
+    console.log(`${key}: ${value}`);
+});
+
+
+// Set
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+
+currenciesUnique.forEach(function(value, key, map) {
+    console.log(`${key} : ${value}`);
+}); // key and value are the same
