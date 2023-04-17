95693aff09fad9d008fa059085cb896b74f03de2
Milana <milana.zaborovska@gmail.com>
2023-04-17 21:10:11 +0100

    findIndex complete

---
+++ Javascript/Arrays/findIndex method.js
@@ -0,0 +1,6 @@
+const array1 = [ 2, 4,6,7,8,0,10,11,23,4,6,19,23,1];
+
+const smallNumber = array1.findIndex( num => num > 20);
+console.log(smallNumber);
+
+// Like find method but instead finds the index
