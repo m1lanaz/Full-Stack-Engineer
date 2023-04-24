deafce221df809aeb1f2a09e9f885040edd2dc91
Milana <milana.zaborovska@gmail.com>
2023-04-24 09:24:47 +0100

    complete promise

--- Javascript/Asynch/iceCreamChallenge.js
+++ Javascript/Asynch/iceCreamChallenge.js
@@ -5,40 +5,56 @@ let stocks = {
     toppings: ['chocolate', 'peanuts'],
 };
 
-let order = (fruit_name, call_production) => {
+let is_shop_open = true;
 
-    setTimeout(function(){
-        console.log(`${stocks.fruits[fruit_name]} was selected`)
+let order = (time,work) => {
+    return new Promise((resolve, reject) => {
+        if(is_shop_open){
+            setTimeout(()=> {
 
-        call_production();
-    }, 2000)
+                resolve( work() )
+            }, time)
+            
+        }
+        else{
+            reject(console.log('Our shop is closed'))
+        }
+    })
+}
 
-};
+order( 2000, ()=>console.log(`${stocks.fruits[0]} was selected`))
 
-let production = () =>{
-
-    setTimeout(()=>{
-      console.log("production has started")
-      setTimeout(()=>{
-        console.log("The fruit has been chopped")
-        setTimeout(()=>{
-          console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`)
-          setTimeout(()=>{
-            console.log("start the machine")
-            setTimeout(()=>{
-              console.log(`Ice cream placed on ${stocks.holder[1]}`)
-              setTimeout(()=>{
-                console.log(`${stocks.toppings[0]} as toppings`)
-                setTimeout(()=>{
-                  console.log("serve Ice cream")
-                },2000)
-              },3000)
-            },2000)
-          },1000)
-        },1000)
-      },2000)
-    },0000)
+.then(()=>{
+    return order(0000,()=>console.log('production has started'))
+  })
   
-  };
 
-order(0,production);
+// step 3
+.then(()=>{
+    return order(2000, ()=>console.log("Fruit has been chopped"))
+  })
+  
+  // step 4
+  .then(()=>{
+    return order(1000, ()=>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`))
+  })
+  
+  // step 5
+  .then(()=>{
+    return order(1000, ()=>console.log("start the machine"))
+  })
+  
+  // step 6
+  .then(()=>{
+    return order(2000, ()=>console.log(`ice cream placed on ${stocks.holder[1]}`))
+  })
+  
+  // step 7
+  .then(()=>{
+    return order(3000, ()=>console.log(`${stocks.toppings[0]} as toppings`))
+  })
+  
+  // Step 8
+  .then(()=>{
+    return order(2000, ()=>console.log("Serve Ice Cream"))
+  })
