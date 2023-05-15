fa9737d72d15164b852fc4af76f037d66f4ef39d
Milana <milana.zaborovska@gmail.com>
2023-05-15 18:38:30 +0100

    set up weekly plan project

---
+++ Javascript/practice/weeklyPlan.js
@@ -0,0 +1,18 @@
+//To do list but with a spin
+// Each to do item will be entered
+// Then the to do item can be moved into a category
+// Item added -> Item is in unorganised section -> Item moved over to the specified section -> Item completed so ticked off -> Whole list can be cleared
+
+let toDoListUnorganise = [];
+let toDoListWeek = [];
+let toDoListMon = [];
+let toDoListTue = [];
+let toDoListWed = [];
+let toDoListThur = [];
+let toDoListFri = [];
+let toDoListWeekends = [];
+
+
+
+
+
